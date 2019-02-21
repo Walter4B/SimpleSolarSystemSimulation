783fa3a56c2fd8bd359226cdb55453099dcba82e
Walter_Ba <walter.bali@outlook.com>
2019-02-21 18:20:44 +0100

    SolarUpload

---
+++ Assets/Scripts/RotateAroundTarget.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateAroundTarget : MonoBehaviour {
+
+    public Transform Target;
+    public Vector3 Axis;
+    public float Speed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.RotateAround (Target.position, Axis, Speed * Time.deltaTime);
+	}
+}
